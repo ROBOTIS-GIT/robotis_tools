0b58e2373be70e812283391bc8f4baaeeeec2695
Pyo <pyo@robotis.com>
2019-06-17 11:55:10 +0900

    Update install_ros_kinetic_rp3.sh

--- install_ros_kinetic_rp3.sh
+++ install_ros_kinetic_rp3.sh
@@ -32,7 +32,7 @@ fi
 echo "[Download the ROS keys]"
 roskey=`apt-key list | grep "ROS Builder"`
 if [ -z "$roskey" ]; then
-  sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 fi
 
 echo "[Check the ROS keys]"
