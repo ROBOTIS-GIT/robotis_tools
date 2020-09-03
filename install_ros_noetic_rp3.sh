54bca7a9a02f9856061578ac0a2eacef7922f410
Will Son <willson@robotis.com>
2020-09-03 12:56:12 +0900

    melodic & noetic scripts

---
+++ install_ros_noetic_rp3.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2017, ROBOTIS CO., LTD.
+
+echo ""
+echo "[Note] Target OS version  >>> Ubuntu Mate for the Raspberry Pi"
+echo "[Note] Target ROS version >>> ROS Noetic Ninjemys"
+echo "[Note] Catkin workspace   >>> $HOME/catkin_ws"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Set the target OS, ROS version and name of catkin workspace]"
+name_os_version=${name_os_version:="focal"}
+name_ros_version=${name_ros_version:="noetic"}
+name_catkin_workspace=${name_catkin_workspace:="catkin_ws"}
+
+echo "[Update the package lists and upgrade them]"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "[Install build environment, the chrony, ntpdate and set the ntpdate]"
+sudo apt-get install -y chrony ntpdate build-essential
+sudo ntpdate ntp.ubuntu.com
+
+echo "[Add the ROS repository]"
+if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
+  sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${name_os_version} main\" > /etc/apt/sources.list.d/ros-latest.list"
+fi
+
+echo "[Download the ROS keys]"
+roskey=`apt-key list | grep "Open Robotics"`
+if [ -z "$roskey" ]; then
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+fi
+
+echo "[Check the ROS keys]"
+roskey=`apt-key list | grep "Open Robotics"`
+if [ -n "$roskey" ]; then
+  echo "[ROS key exists in the list]"
+else
+  echo "[Failed to receive the ROS key, aborts the installation]"
+  exit 0
+fi
+
+echo "[Update the package lists and upgrade them]"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "[Install the ros-base]"
+sudo apt-get install -y ros-$name_ros_version-ros-base
+
+echo "[Environment setup and getting rosinstall]"
+source /opt/ros/$name_ros_version/setup.sh
+
+echo "[Setup 1GB Swap Partition]"
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sh -c "echo \"/swapfile swap swap defaults 0 0\" >> /etc/fstab"
+
+echo "[Make the catkin workspace and test the catkin_make]"
+mkdir -p $HOME/$name_catkin_workspace/src
+cd $HOME/$name_catkin_workspace/src
+catkin_init_workspace
+cd $HOME/$name_catkin_workspace
+catkin_make
+
+echo "[Set the ROS evironment]"
+sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
+sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
+sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
+
+sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+
+sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
+sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+
+source $HOME/.bashrc
+
+echo "[Complete!!!]"
+exit 0
