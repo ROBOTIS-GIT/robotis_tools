d95ed9534d84bc758ec94c4ba8e9cf759627e211
Will Son <willson@robotis.com>
2021-04-06 17:12:25 +0900

    revise foxy script

--- install_ros2_foxy.sh
+++ install_ros2_foxy.sh
@@ -15,15 +15,16 @@ echo "[Set the target ROS version and name of colcon workspace]"
 name_ros_version=${name_ros_version:="foxy"}
 name_colcon_workspace=${name_colcon_workspace:="colcon_ws"}
 
-echo "[Setup Locale]"
+echo "[Set Locale]"
+sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 echo "[Setup Sources]"
 sudo rm -rf /var/lib/apt/lists/* && sudo apt update && sudo apt install -y curl gnupg2 lsb-release
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
 echo "[Install ROS 2 packages]"
 sudo apt update && sudo apt install -y ros-$name_ros_version-desktop
